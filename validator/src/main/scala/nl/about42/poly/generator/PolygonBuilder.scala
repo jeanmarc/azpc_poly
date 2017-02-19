2b2f49c98071bd9b23bc58515282297713fa6d68
Jean-Marc van Leerdam <jean.marc.van.leerdam@ing.nl>
2017-02-19 14:26:00 +0100

    Improved rendering, optimize early-abandon (for larger polys)

--- validator/src/main/scala/nl/about42/poly/generator/PolygonBuilder.scala
+++ validator/src/main/scala/nl/about42/poly/generator/PolygonBuilder.scala
@@ -170,7 +170,7 @@ class PolygonState(val currentPath: Path, val remainingX: Seq[Int], val remainin
             val remX = remainingX.filter(_ != vertex.x)
             val remY = remainingY.filter(_ != vertex.y)
             val newPath = new Path(currentPath.vertices :+ vertex)
-            if (remX.size == 4){
+            if (remX.size == 5){
               if (closingPossible(newPath, remX, remY)) {
                 Some(new PolygonState(newPath, remX, remY))
               } else {
